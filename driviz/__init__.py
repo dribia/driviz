9cb8acab7830f00149fde5f516565a4ff97b3d1e
Helena Ribera <helena@dribia.com>
2024-10-24 16:47:53 +0200

    Move `theme` to `__init__.py`.

--- driviz/__init__.py
+++ driviz/__init__.py
@@ -1,10 +1,11 @@
 """DriViz."""
 
 from importlib.metadata import PackageNotFoundError, version
+from .theme import theme
 
 try:
     __version__ = version(__name__)
 except PackageNotFoundError:
     __version__ = "unknown"
 
-__all__ = ["__version__"]
+__all__ = ["__version__", "theme"]
