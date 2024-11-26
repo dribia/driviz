cdc513f2d1ea9223d34d19000b8a838d328e5be8
mauro <106584676+mauro-dribia@users.noreply.github.com>
2024-11-26 11:01:24 +0100

    :memo: add more usage examples

--- scripts/examples/usage_six_colors.py
+++ scripts/examples/usage_six_colors.py
@@ -8,5 +8,8 @@ theme.set_basic_colors(six=True)
 source = pd.DataFrame(
     data={"category": [1, 2, 3, 4, 5, 6], "value": [4, 6, 10, 3, 7, 8]},
 )
-
-chart = alt.Chart(source).mark_arc().encode(theta="value", color="category:N")
+chart = (
+    alt.Chart(source)
+    .mark_arc()
+    .encode(theta="value", color=alt.Color("category:N", title="Category"))
+)
