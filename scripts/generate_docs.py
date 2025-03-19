6671ebdc47e4c5671381117e2e22f1fd760d888a
Mauro Escobar <mauro@dribia.com>
2025-03-19 15:08:02 +0100

    :wrench: plot in png as well

--- scripts/generate_docs.py
+++ scripts/generate_docs.py
@@ -11,18 +11,15 @@ def add_plot(name: str) -> str:
     """Add the plot to the code."""
     with (Path(__file__).parents[0] / "examples" / f"{name}.py").open() as file:
         code = file.read()
-
     chart = importlib.import_module(f"scripts.examples.{name}").chart
-
-    output_dir = Path(__file__).parents[1] / "docs" / "plots"
-    output_dir.mkdir(exist_ok=True)
-
-    img_format = "png"
-    output_path = output_dir / f"{name}.{img_format}"
-
-    chart.save(str(output_path))
-
-    return f"```python\n{code}\n```\n\n![Plot](plots/{name}.{img_format})\n"
+    if "png" in name:
+        output_dir = Path(__file__).parents[1] / "docs" / "plots"
+        output_dir.mkdir(exist_ok=True)
+        chart.save(str(output_dir / f"{name}.png"))
+        embedded_plot = f"![Plot](plots/{name}.png)\n"
+    else:
+        embedded_plot = f"```vegalite\n{chart.to_json(indent=2)}\n```\n"
+    return f"```python\n{code}\n```\n\n{embedded_plot}"
 
 
 if __name__ == "__main__":
@@ -30,16 +27,16 @@ if __name__ == "__main__":
         loader=FileSystemLoader(Path(__file__).parents[1] / "docs" / "templates")
     )
 
-    for name in jinja_env.list_templates():
-        template = jinja_env.get_template(name)
+    for name_ in jinja_env.list_templates():
+        template = jinja_env.get_template(name_)
         render = template.render(
             **{
                 f.name.replace(".py", ""): add_plot(f.name.replace(".py", ""))
                 for f in (Path(__file__).parents[0] / "examples").glob("*.py")
-                if f.name.startswith(name[0 : name.find(".")])
+                if f.name.startswith(name_[0 : name_.find(".")])
             }
         )
-        with Path(Path(__file__).parents[1] / "docs" / name.replace(".jinja", "")).open(
-            mode="w"
-        ) as f:
+        with Path(
+            Path(__file__).parents[1] / "docs" / name_.replace(".jinja", "")
+        ).open(mode="w") as f:
             f.write(render)
